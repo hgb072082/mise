904d83066e1f1f9be583a2910b7ba30745962338
hgb072082 <hgb072082@gmail.com>
2022-04-09 21:10:47 +0900

    edit deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Main from '../src/component/units/main/Main.Container';
+import Main from '../src/component/units/main/Main.container';
 
 export default function Home() {
   return <Main />;
