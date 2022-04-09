90f9a7a280bc71602b8e166c52c3c46b14751175
hgb072082 <hgb072082@gmail.com>
2022-04-09 20:58:49 +0900

    deploy

--- src/component/units/fetch/Fetch.container.tsx
+++ src/component/units/fetch/Fetch.container.tsx
@@ -20,3 +20,27 @@ export default function Fetch() {
     />
   );
 }
+
+// function Example() {
+//   const { isLoading, error, data, isFetching } = useQuery("repoData", () =>
+//     fetch(
+//       "https://api.github.com/repos/tannerlinsley/react-query"
+//     ).then((res) => res.json())
+//   );
+
+//   if (isLoading) return "Loading...";
+
+//   if (error) return "An error has occurred: " + error.message;
+
+//   return (
+//     <div>
+//       <h1>{data.name}</h1>
+//       <p>{data.description}</p>
+//       <strong>👀 {data.subscribers_count}</strong>{" "}
+//       <strong>✨ {data.stargazers_count}</strong>{" "}
+//       <strong>🍴 {data.forks_count}</strong>
+//       <div>{isFetching ? "Updating..." : ""}</div>
+//       <ReactQueryDevtools initialIsOpen />
+//     </div>
+//   );
+// }
